5e1f10eecd8190ee02c71657bbc26dd23272989e
Nicole Zheng <zznicole@hotmail.com>
2020-04-28 00:00:14 +0200

    solve playlist and  modalbox

--- modalBox/src/script.js
+++ modalBox/src/script.js
@@ -1,9 +1,17 @@
 // 2. Create an HTML page with OPEN button and a modal box. The modal box should have text and a CLOSE button. The modal box is not displayed from the beginning. It appears when clicking the OPEN button and disappears when clicking CLOSE.
 
-let modal = document.getElementById("myModal");
-let btn = document.getElementById("openBtn");
+let modal = document.getElementById("modalBox");
+let open = document.getElementById("openBtn");
 let close = document.getElementById("closeBtn");
 
-open() {
+open.onclick = function() {
   modal.style.display = "block";
+}
+close.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if(event.target == modal) {
+    modal.style.display = "none";
+  }
 }
